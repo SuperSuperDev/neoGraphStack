ee0417219b7e291e083b75fe1ee7a8ecbf0725f8
SuperSuperStore <steven@supersuperstore.com>
2021-11-19 21:07:42 +0000

    Installed NextJS and updated Docker

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,6 @@
-const { Neo4jGraphQL } = require("@neo4j/graphql");
-const { ApolloServer, gql } = require("apollo-server");
-const neo4j = require("neo4j-driver");
+const { Neo4jGraphQL } = require('@neo4j/graphql')
+const { ApolloServer, gql } = require('apollo-server')
+const neo4j = require('neo4j-driver')
 
 const typeDefs = gql`
   type Site {
@@ -15,21 +15,20 @@ const typeDefs = gql`
     content: String
     site: [Site] @relationship(type: "HAS_PAGE", direction: IN)
   }
-`;
+`
 
 const driver = neo4j.driver(
-  "bolt://localhost:7687",
-  neo4j.auth.basic("neo4j", "letmein"),
+  'bolt://localhost:7687',
+  neo4j.auth.basic('neo4j', 'letmein'),
   { encrypted: false }
-);
+)
 
-const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver })
 
 const server = new ApolloServer({
-  
   schema: neoSchema.schema,
-});
+})
 
 server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+  console.log(`🚀 Server ready at ${url}`)
+})
